ee4e469ce762e1e479167b3fcfc219f99bc9dee1
Daniil Mur <110899107+KGP85@users.noreply.github.com>
2025-04-18 11:09:13 +0300

    Add files via upload

---
+++ Module_22HW/CircleGame/CircleGameTests/CircleGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  CircleGameTests.swift
+//  CircleGameTests
+//
+//  Created by Даниил Муратович on 12.04.2025.
+//
+
+import Testing
+@testable import CircleGame
+
+struct CircleGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
